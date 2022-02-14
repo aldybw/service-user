497e54696278912da97c203ca38bb127d9f8ecbe
Aldycky1 <bagusaldycky@gmail.com>
2022-02-14 13:06:15 +0700

    created user-seeder

--- seeders/20220214055134-user-seeders.js
+++ seeders/20220214055134-user-seeders.js
@@ -1,24 +1,35 @@
-'use strict';
+"use strict";
+const bcrypt = require("bcrypt");
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "Aldy",
+          profession: "Back End Developer",
+          role: "admin",
+          email: "aldy@gmail.com",
+          password: await bcrypt.hash("aldyadmin123", 10),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: "Bagus",
+          profession: "Front End Developer",
+          role: "student",
+          email: "bagus@gmail.com",
+          password: await bcrypt.hash("bagusstudent123", 10),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("users", null, {});
+  },
 };
